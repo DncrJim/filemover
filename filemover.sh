2c6c16a3a1f29b42d0b0d98588861723bac9a9fd
Jim Dersch <dncrjim@gmail.com>
2022-06-29 21:25:51 -0400

    i broke it

--- filemover.sh
+++ filemover.sh
@@ -4,17 +4,17 @@
     if [ ! -f "filemover_config.sh" ] ; then
       echo "host_url='sftp://ftp.domain.com'\nhost_user=''\nhost_pass=''\n\nremote_dir=''  #unknown if needs trailing slash\nlocal_dir=''  #needs trailing slash\n\npath_to_list='filemover.list'  #should include the file name you want to use\npath_to_queue='filemover.queue'  #should include the file name you want to use" > filemover_config.sh
       echo "filemover_config.sh created, please edit the file to input your variables"
-      exit 0
-    fi
+     exit 0
+   fi
 
 #load config file variables
-    . ~/filemover_config.sh
+   . ~/filemover_config.sh
 
 #if filemover.list doesn't exist, create it
-    if [ ! -f "$path_to_list" ] ; then
-      touch $path_to_list
-      echo "filemover.list created"
-    fi
+   if [ ! -f "$path_to_list" ] ; then
+     touch $path_to_list
+     echo "filemover.list created"
+   fi
 
 #get remote list of files in directory
 lftp -u $host_user,$host_pass $host_url <<EOF
@@ -30,21 +30,42 @@ EOF
 rm filemover.temp
 
 #download files in queue
-lftp -u $host_user,$host_pass $host_url <<EOF
+while true
+  do
+        read -r filepath < "$path_to_queue"
 
-mirror "$remote_dir" $local_dir;
+      #if $filepath is empty, break loop
+      if [[ -z "$filepath" ]] ; then
+          break
+      else
 
-exit
-EOF
+      #if filepath has one of these endings, use get, otherwise use mirror.
+          if [[ $file == *.mkv || $file == *.mp4 ]] ; then
+            # || ( $file == *.avi ) || ( $file == *.webm ) || ( $file == *.flv ) || ( $file == *.vob ) || ( $file == *.mts ) || ( $file == *.m2ts ) || ( $file == *.ts ) || ( $file == *.mov ) || ( $file == *.wmv ) || ( $file == *.m4p ) || ( $file == *.m4v ) || ( $file == *.mpg ) || ( $file == *.mpeg )
+              echo "determined to be file, using get"
+              lftp -u $host_user,$host_pass -e "get -c '$remote_dir$filepath' -o '$local_dir$filepath';quit;" $host_url
+          else
+              echo "determined to be folder, using mirror"
+              lftp -u $host_user,$host_pass -e "mirror -c --parallel=3 --verbose '$remote_dir$filepath' $local_dir;quit;" $host_url
+          fi
+      fi
+    #confirm no error on transfer
+      if [ $? -ne 0 ] ; then
+          echo "error mv $filepath"
+        else
+          echo "success $filepath"
+          echo "$filepath" >> $path_to_list
+        #delete line from queue
+          sed -i 1d $path_to_queue
+      fi
+    done
 
 
-  #determine if file or folders
-  #if file use get command
-  #if folder use mirror command
 
-  #confim if file downloaded correctly?
-  #remove file/folder from queue and add to filemover.list
+		if [ ! -s $path_to_queue ] ; then
+      echo "queue is empty!"
+    else
+      echo "queue is not empty!"
+    fi
 
-#wait preset time before repeating from top - maybe 10 minutes?
-#sleep 60
-#repeat
+exit 0
